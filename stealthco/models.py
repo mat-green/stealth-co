7ce510a3d18b5072af1d8e45432937031fa622be
Matthew Green <matthew@newedgeengineering.com>
2016-01-24 14:36:53 +0000

    basic attempt at plotting lines

--- stealthco/models.py
+++ stealthco/models.py
@@ -10,7 +10,7 @@ Created on 24 Jan 2016
 
 class Graph(object):
     '''
-    classdocs
+    Object that plots vectors as lines and render to ascii characters.
     '''
 
     def __init__(self, width, height):
@@ -31,8 +31,92 @@ class Graph(object):
             grid.append(row)
             a += 1
         self.grid = grid
+        print("grid is %sx%s" % (len(self.grid[0]), len(self.grid)))
+
+
+    def plot(self, start, end):
+        '''
+        Updated the graph with a plotted line
+        
+        :param start: The start coordinate as a Coordinate
+        :param end: The end coordinate as a Coordinate
+        '''
+        # determine direction
+#         direction = [0,0]
+#         if(start.x > 0 and end.x > 0):
+#             print("%s/%s=%s" % (start.x,end.x,float(start.x)/float(end.x)))
+#             direction[0] = float(start.x)/float(end.x)
+#         if(start.y > 0 and end.y > 0):
+#             print("b")
+#             direction[1] = float(start.y)/float(end.y)
+#             
+#         print("direction: %s" % direction)
+        
+        rise = end.y - start.y
+        run = end.x - start.x
+        print("rise %s, run %s" % (rise, run))
+        if(rise != 0 and run != 0):
+            slope = float(abs(rise)) / float(abs(run))
+        else :
+            slope = 0
+        # insert X into grid
+        x = start.x
+        y = start.y
+#         print("starting: %s,%s @ slope: %s to %s,%s" % (x,y,slope,(end.x),(end.y)))
+        if(rise >= 0):
+            while x <= (end.x) and y <= (end.y):
+                print("setting x: %s, y: %s to X" % (x, y))
+                self.grid[y][x] = "X"
+                if(slope > 0):
+                    if(slope < 1):
+                        x += int(round(slope, 0))
+                    else:
+                        x += int(slope)
+                    y += 1
+                else: # use rise and run to determine direction.
+                    if(rise > 0):
+                        y += 1
+                    else:
+                        x += 1
+        elif(rise <= 0):
+            while x <= (end.x) and y >= (end.y):
+                print("setting x: %s, y: %s to X" % (x, y))
+                self.grid[y][x] = "X"
+                if(slope > 0):
+                    if(slope < 1):
+                        x += int(round(slope, 0))
+                    else:
+                        x += int(slope)
+                    y -= 1
+                else: # use rise and run to determine direction.
+                    if(rise < 0):
+                        y -= 1
+                    else:
+                        x += 1
 
 
     def render(self):
+        '''
+        Basic render of data.
+        
+        :return: Ascii string of characters
+        '''
         result = [ ''.join(line) for line in self.grid ]
         return '\n'.join(result)
+
+
+
+class Coordinate(object):
+    '''
+    Holds coordinates
+    '''
+
+    def __init__(self, x, y):
+        '''
+        Constructor.
+
+        :param x: X position as a Integer.
+        :param y: Y position as a Integer.
+        '''
+        self.x = x
+        self.y = y
--- stealthco/tests.py
+++ stealthco/tests.py
@@ -9,7 +9,7 @@ Created on 24 Jan 2016
 '''
 import unittest
 
-from models import Graph
+from models import Graph, Coordinate
 
 class TestGrid(unittest.TestCase):
 
@@ -28,7 +28,121 @@ class TestGrid(unittest.TestCase):
         # test execution
         actual = component.render()
         # test verification
-        self.assertEqual("                    \n                    \n                    \n                    \n                    \n                    \n                    \n                    \n                    \n                    ", actual)
+        self.assertEqual(actual, "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    ")
+
+
+    def testDot(self):
+        # test set up
+        component = Graph(20,10)
+        start = Coordinate(1,1)
+        end = Coordinate(1,1)
+        # test execution
+        component.plot(start, end)
+        actual = component.render()
+        # test verification
+        self.assertEqual(actual, "                    \n" \
+                                 " X                  \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    ")
+
+
+    def testHorizontal(self):
+        # test set up
+        component = Graph(20,10)
+        start = Coordinate(0,4)
+        end = Coordinate(19,4)
+        # test execution
+        component.plot(start, end)
+        actual = component.render()
+        # test verification
+        self.assertEqual(actual, "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "XXXXXXXXXXXXXXXXXXXX\n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    \n" \
+                                 "                    ")
+
+
+    def testVertical(self):
+        # test set up
+        component = Graph(20,10)
+        start = Coordinate(1,1)
+        end = Coordinate(1, 8)
+        # test execution
+        component.plot(start, end)
+        actual = component.render()
+        # test verification
+        self.assertEqual(actual, "                    \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 " X                  \n" \
+                                 "                    ")
+
+
+    def testDiagonal(self):
+        # test set up
+        component = Graph(20,10)
+        start = Coordinate(1,1)
+        end = Coordinate(8, 8)
+        # test execution
+        component.plot(start, end)
+        actual = component.render()
+        # test verification
+        self.assertEqual(actual, "                    \n" \
+                                 " X                  \n" \
+                                 "  X                 \n" \
+                                 "   X                \n" \
+                                 "    X               \n" \
+                                 "     X              \n" \
+                                 "      X             \n" \
+                                 "       X            \n" \
+                                 "        X           \n" \
+                                 "                    ")
+
+
+    def testBackDiagonal(self):
+        # test set up
+        component = Graph(20,10)
+        start = Coordinate(4,9)
+        end = Coordinate(15, 0)
+        # test execution
+        component.plot(start, end)
+        actual = component.render()
+        # test verification
+        self.assertEqual(actual, "             X      \n" \
+                                 "          X         \n" \
+                                 "         X          \n" \
+                                 "        X           \n" \
+                                 "       X            \n" \
+                                 "      X             \n" \
+                                 "     X              \n" \
+                                 "    X               \n" \
+                                 "   X                \n" \
+                                 "                    ")
 
 
 if __name__ == "__main__":
